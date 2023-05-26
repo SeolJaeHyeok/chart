66b29b62b04fb3522d4c6f11b53eed025adad141
hovelopin <kimhojin3714@naver.com>
2023-05-26 14:53:28 +0900

    feat : chartjs , echart 추가

--- src/pages/chartjs/index.tsx
+++ src/pages/chartjs/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -14,38 +14,62 @@ import { Line } from 'react-chartjs-2';
 
 ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Filler, Legend);
 
-const options = {
-  responsive: true,
-  plugins: {
-    legend: {
-      display: false,
-    },
-  },
-  elements: {
-    tension: 0.4,
-  },
-};
-
-// const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-const labels = ['0시', '6시', '12시', '15시', '18시', '21시', '24시'];
-
-// data에 값을 넣을때 무조건 labels라는 이름으로 값을 넣어야 인식한다.
-const data = {
-  labels,
-  datasets: [
-    {
-      // 그래프내에 그라데이션 효과를 넣어준다.
-      fill: true,
-      label: '혈당',
-      // data에 label에 맞게 값을 넣어놓으면 된다.
-      data: [70, 40, 200, 50, 49, 180, 60],
-      borderColor: 'rgb(53, 162, 235)',
-      backgroundColor: 'rgba(53, 162, 235, 0.5)',
-    },
-  ],
-};
-
 export default function Chartjs() {
+  const ref = useRef<any>();
+
+  const options = {
+    responsive: true,
+    plugins: {
+      //그래프에 나와있는 라벨을 지우는 속성
+      legend: {
+        display: false,
+      },
+    },
+    scales: {
+      y: {
+        min: 0,
+        max: 250,
+        ticks: {
+          stepSize: 50,
+        },
+      },
+    },
+  };
+
+  const times = ['0시', '6시', '12시', '15시', '18시', '21시', '24시'];
+
+  const data = {
+    labels: times,
+    datasets: [
+      {
+        // 그래프내에 그라데이션 효과를 넣어준다.
+        fill: {
+          target: 'start',
+        },
+        label: '혈당',
+        // data에 label에 맞게 값을 넣어놓으면 된다.
+        // null값으로 값을 넣으면 끊어져서 보인다.
+        data: [70, 40, 200, 50, 49, 180, 60],
+        borderColor: 'rgb(53, 162, 235)',
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+        // 그래프를 곡선으로 만들어주는 용도
+        tension: 0.4,
+        pointStyle: (ctx: any) => {
+          if (ctx.parsed.y >= 100) {
+            return ref.current;
+          } else {
+            return 'circle';
+          }
+        },
+      },
+    ],
+  };
+
+  useEffect(() => {
+    ref.current = new Image(25, 25);
+    ref.current.src = '/health_icon.svg';
+  }, []);
+
   return (
     <div
       style={{
